2576edebe3ee7ce8714a889968fcfaf976b13fc9
spoyodevelop <kjy9505@gmail.com>
2025-04-23 18:52:46 +0900

    불필요한 주석 코드 제거

--- 10814-나이순_정렬/10814.js
+++ 10814-나이순_정렬/10814.js
@@ -9,16 +9,5 @@ const arr = strings.slice(1).map((str) => {
 });
 arr.sort((a, b) => a.age - b.age);
 
-// 안쓰는 코드는 지우는게 좋은 습관입니다.
-// 안쓰는 코드는 지우는게 좋은 습관입니다.
-// 근데 나중에 써먹을거 같긴 해서 남겨둠.
-// arr.sort((a, b) => {
-//   if (a.age > b.age) return 1;
-//   if (a.age < b.age) return -1;
-//   if (a.name < b.name) return 1;
-//   if (a.name > b.name) return -1;
-//   return 0;
-// });
-
 const output = arr.map((item) => `${item.age} ${item.name}`).join("\n");
 console.log(output);
