1ccb5b0cf846eef28257a459b6c279e7696a42a5
spoyodevelop <kjy9505@gmail.com>
2025-08-03 19:46:21 +0900

    캐시 구현: Map에서 Set으로 변경하여 성능 개선

--- 레벨3임시/4주차/캐시/캐시.js
+++ 레벨3임시/4주차/캐시/캐시.js
@@ -2,20 +2,20 @@ function solution(cacheSize, cities) {
   if (cacheSize === 0) return cities.length * 5;
 
   let time = 0;
-  const cache = new Map();
+  const cache = new Set();
   for (const city of cities) {
     const raw = city.toLowerCase();
     if (cache.has(raw)) {
       time++;
       cache.delete(raw);
-      cache.set(raw, true);
+      cache.add(raw);
     } else {
       time += 5;
       if (cacheSize === cache.size) {
-        const target = cache.keys().next().value;
+        const target = cache.values().next().value;
         cache.delete(target);
       }
-      cache.set(raw, true);
+      cache.add(raw);
     }
   }
   return time;
