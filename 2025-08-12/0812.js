831bc702af495430f2e016000dcda0bbaa4f5876
deokbin <deokbin@naver.com>
2025-08-12 21:07:26 +0900

    solved: 0812 algorithm

---
+++ 2025-08-12/0812.js
@@ -0,0 +1,41 @@
+function solution(dirs) {
+  let dx = 0;
+  let dy = 0;
+  const direction = {
+    U: [0, 1],
+    D: [0, -1],
+    R: [1, 0],
+    L: [-1, 0],
+  };
+
+  const visited = new Set();
+  let count = 0;
+
+  for (const dir of dirs) {
+    const lastX = dx;
+    const lastY = dy;
+    const [moveX, moveY] = direction[dir];
+    const newX = lastX + moveX;
+    const newY = lastY + moveY;
+
+    if (newX < -5 || newX > 5 || newY < -5 || newY > 5) {
+      continue;
+    }
+
+    // 순서 바뀌어도 같은 경로라서 정렬 해서 정리해야 함
+
+    const path1 = `${lastX},${lastY},${newX},${newY}`;
+    const path2 = `${newX},${newY},${lastX},${lastY}`;
+
+    if (!visited.has(path1) && !visited.has(path2)) {
+      visited.add(path1);
+      visited.add(path2);
+      count++;
+    }
+
+    dx = newX;
+    dy = newY;
+  }
+
+  return count;
+}
