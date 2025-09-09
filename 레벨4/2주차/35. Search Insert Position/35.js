62d642508a0fdf0e62f910028b8a5f09aa461dbe
spoyodevelop <kjy9505@gmail.com>
2025-09-09 21:48:24 +0900

    35. Search Insert Position 문제를 위한 함수 및 설명 문서 추가

---
+++ 레벨4/2주차/35. Search Insert Position/35.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length;
+  let mid;
+  while (left < right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] > target) right = mid;
+    else left = mid + 1;
+  }
+  return left;
+};
