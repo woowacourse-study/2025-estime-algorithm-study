dd4dba6ee126f94984e58694130d2e153dc21a3b
spoyodevelop <kjy9505@gmail.com>
2025-09-28 22:07:23 +0900

    정규 표현식 매칭 문제를 위한 함수 및 설명 문서 추가

---
+++ 레벨4/4주차/regular-expression-matching/regular-expression-matching.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+  const reg = new RegExp(`^${p}$`);
+  return reg.test(s);
+};
---
+++ 레벨4/4주차/regular-expression-matching/regular-expression-matching.md
@@ -0,0 +1,13 @@
+## 10.regular-expression-matching
+
+## 풀이 날짜(9/28)
+
+## 풀이 시간
+
+20분.
+
+## 풀이
+
+string => regExp로 어떻게 파싱할지 물어보는 문제.
+
+new RegExp(`^${a}$`) 으로 앞과끝을 주면 된다.
